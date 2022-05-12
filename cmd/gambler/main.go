b4d5011a1a733cd64882bf3b9174e5c41435c552
nanchano <nicolas.anchanio@gmail.com>
2022-05-12 19:49:49 -0300

    add date range

--- cmd/gambler/main.go
+++ cmd/gambler/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/nanchano/gambler/pkg/exchanges"
 )
 
 func main() {
 	id := "ethereum"
-	date := "20-04-2022"
-	h := exchanges.NewCoingeckoHandler(id, date)
-	// b := h.Extract()
-	// t := h.Transform(b)
-	// fmt.Println(t)
-	h.ResponseConsumer()(h.ResponseProcessor()(h.ResponseGenerator()()))
+	// date := "20-04-2022"
+	// h := exchanges.NewCoingeckoHandler(id, date)
+	// h.ResponseConsumer()(h.ResponseProcessor()(h.ResponseGenerator()()))
+	// start := time.Now()
+	// end := start.AddDate(0, 1, 0)
+	start, _ := time.Parse("02-01-2006", "01-04-2022")
+	end, _ := time.Parse("02-01-2006", "01-05-2022")
+	for d := start; d.After(end) == false; d = d.AddDate(0, 0, 1) {
+		ds := d.Format("02-01-2006")
+		fmt.Println(ds)
+		h := exchanges.NewCoingeckoHandler(id, ds)
+		h.ResponseConsumer()(h.ResponseProcessor()(h.ResponseGenerator()()))
+	}
 }
