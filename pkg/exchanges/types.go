91e0642e83836259f349568feaf0eb55fb09fb0d
nanchano <nicolas.anchanio@gmail.com>
2022-05-17 18:39:41 -0300

    remove types

--- pkg/exchanges/types.go
+++
@@ -1,9 +0,0 @@
-package exchanges
-
-type Response map[string]interface{}
-
-type Extractor func() <-chan []byte
-
-type Processor func(<-chan []byte) <-chan *Response
-
-type Consumer func(<-chan *Response)
