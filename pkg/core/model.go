5e6bcef93eeb9ee5f478c756b8d9a247279229f0
nanchano <nicolas.anchanio@gmail.com>
2022-05-20 12:39:59 -0300

    service migration part 1

--- pkg/core/model.go
+++ pkg/core/model.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-// GamblerResponse represents a normalized response from any crypto API
+// GamblerEvent represents a normalized response from any crypto API
 type GamblerEvent struct {
 	ID        string      `json:"id"`
 	Name      string      `json:"name"`
