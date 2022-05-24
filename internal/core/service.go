07b8e3dec67a591ac4b331c82dae755a48b70990
nanchano <nicolas.anchanio@gmail.com>
2022-05-23 21:56:03 -0300

    adapt repo interface

--- internal/core/service.go
+++ internal/core/service.go
@@ -1,7 +1,7 @@
 package core
 
 type GamblerService interface {
-	Find(exchange, coin, date string) (*GamblerEvent, error)
+	Find(coin, date string) (*GamblerEvent, error)
 	Store(ge *GamblerEvent) error
 }
 
@@ -18,7 +18,7 @@ func NewGamblerService(gp GamblerPipeline, gr GamblerRepository) GamblerService 
 }
 
 func (gs *gamblerService) Find(coin, date string) (*GamblerEvent, error) {
-	return gs.gamblerRepository.Find(exchange, coin, date)
+	return gs.gamblerRepository.Find(coin, date)
 }
 
 func (gs *gamblerService) Store(event *GamblerEvent) error {
