1eb94dadf632d5ebd0c2b30699ea7bd3ce3b4de0
nanchano <nicolas.anchanio@gmail.com>
2022-05-25 20:09:41 -0300

    table testing

--- pkg/pipeline/coingecko/response_test.go
+++ pkg/pipeline/coingecko/response_test.go
@@ -8,44 +8,79 @@ import (
 )
 
 func TestBuildExtraField(t *testing.T) {
-	r := newTestResponse()
-	name := "Parse Image, MarketData, CommunityData, DeveloperData and PublicInterestStats into an aggregated map"
-	expected := map[string]interface{}{
-		"thumb":         "link_to_thumb",
-		"current_price": map[string]float64{"usd": 123.45},
-		"market_cap":    map[string]float64{"usd": 678.90},
-		"total_volume":  map[string]float64{"usd": 432.10},
-		"facebook":      map[string]int32{"likes": 1234},
-		"stars":         123456,
-		"alexa_rank":    3000,
+	cases := []struct {
+		name     string
+		response *coingeckoResponse
+		expected map[string]interface{}
+	}{
+		{
+			name:     "Has data",
+			response: newTestResponse(),
+			expected: map[string]interface{}{
+				"thumb":         "link_to_thumb",
+				"current_price": map[string]float64{"usd": 123.45},
+				"market_cap":    map[string]float64{"usd": 678.90},
+				"total_volume":  map[string]float64{"usd": 432.10},
+				"facebook":      map[string]int32{"likes": 1234},
+				"stars":         123456,
+				"alexa_rank":    3000,
+			},
+		},
+		{
+			name:     "Empty response",
+			response: &coingeckoResponse{},
+			expected: map[string]interface{}{},
+		},
 	}
-	t.Run(name, func(t *testing.T) {
-		actual := r.buildExtraField()
-		if !reflect.DeepEqual(actual, expected) {
-			t.Fail()
-		}
-	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := c.response.buildExtraField()
+			if !reflect.DeepEqual(actual, c.expected) {
+				t.Fail()
+			}
+		})
+	}
 }
 
 func TestConvert(t *testing.T) {
 	r := newTestResponse()
-	name := "Convert the coingeckoResponse into a GamblerEvent"
-	expected := core.GamblerEvent{
-		ID:        r.ID,
-		Name:      r.Name,
-		Symbol:    r.Symbol,
-		Date:      r.Date,
-		Price:     r.MarketData["current_price"]["usd"],
-		MarketCap: r.MarketData["market_cap"]["usd"],
-		Volume:    r.MarketData["total_volume"]["usd"],
-		Extra:     r.buildExtraField(),
+	cases := []struct {
+		name     string
+		response *coingeckoResponse
+		expected *core.GamblerEvent
+	}{
+		{
+			name:     "Has data",
+			response: r,
+			expected: &core.GamblerEvent{
+				ID:        r.ID,
+				Name:      r.Name,
+				Symbol:    r.Symbol,
+				Date:      r.Date,
+				Price:     r.MarketData["current_price"]["usd"],
+				MarketCap: r.MarketData["market_cap"]["usd"],
+				Volume:    r.MarketData["total_volume"]["usd"],
+				Extra:     r.buildExtraField(),
+			},
+		},
+		{
+			name:     "Empty response",
+			response: &coingeckoResponse{},
+			expected: &core.GamblerEvent{
+				Extra: map[string]interface{}{},
+			},
+		},
 	}
-	t.Run(name, func(t *testing.T) {
-		actual := r.Convert()
-		if !reflect.DeepEqual(actual, &expected) {
-			t.Fail()
-		}
-	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := c.response.Convert()
+			if !reflect.DeepEqual(actual, c.expected) {
+				t.Fail()
+			}
+		})
+	}
 }
 
 func newTestResponse() *coingeckoResponse {
