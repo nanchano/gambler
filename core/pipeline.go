62b5a0f362d35e10f4d15e1db4c81ac918f5f8a2
nanchano <nicolas.anchanio@gmail.com>
2022-06-24 17:21:50 -0300

    docstrings

--- core/pipeline.go
+++ core/pipeline.go
@@ -1,11 +1,11 @@
 package core
 
-// Generic response from any API Pipeline. Must convert itself into a GamblerEvent
+// PipelineResponse implementations are responses from any API Pipeline. Must convert itself into a GamblerEvent
 type PipelineResponse interface {
 	Convert() *GamblerEvent
 }
 
-// Generic Pipeline, must extract data for a set of dates, and process the responses into GamblerEvents
+// GamblerPipeline implementations must extract data for a set of dates, and process the responses into GamblerEvents
 type GamblerPipeline interface {
 	Extract(dates ...string) <-chan PipelineResponse
 	Process(responses <-chan PipelineResponse) <-chan *GamblerEvent
