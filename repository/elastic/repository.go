62a287af0e4af113152bcaf465e5ce606951cef6
nanchano <nicolas.anchanio@gmail.com>
2022-06-27 23:07:36 -0300

    public repo, fix imports, Store just saves one event

--- repository/elastic/repository.go
+++ repository/elastic/repository.go
@@ -12,11 +12,11 @@ import (
 
 	elastic "github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
-	"github.com/nanchano/gambler/internal/core"
+	"github.com/nanchano/gambler/core"
 )
 
 // repository will implement the core.GamblerRepository interface for the Elastic Repository
-type repository struct {
+type Repository struct {
 	client *elastic.Client
 }
 
@@ -38,19 +38,19 @@ func newElasticClient() (*elastic.Client, error) {
 }
 
 // NewRepository creates a new repository that implements core.GamblerRepository
-func NewRepository() core.GamblerRepository {
+func NewRepository() *Repository {
 	client, err := newElasticClient()
 	if err != nil {
 		log.Fatalf("Error initializing the ElasticSearch client: %s", err)
 	}
 
-	return &repository{
+	return &Repository{
 		client: client,
 	}
 }
 
 // Find returns a core.GamblerEvent from the ElasticSearch repository based on the coin and date provided
-func (r *repository) Find(coin, date string) (*core.GamblerEvent, error) {
+func (r *Repository) Find(coin, date string) (*core.GamblerEvent, error) {
 	var event core.GamblerEvent
 	ctx := context.Background()
 	var buf bytes.Buffer
@@ -91,34 +91,29 @@ func (r *repository) Find(coin, date string) (*core.GamblerEvent, error) {
 }
 
 // Store saves the core.GamblerEvents provided into the ElasticSearch instance
-func (r *repository) Store(events <-chan *core.GamblerEvent) error {
-	for {
-		event, ok := <-events
-		if !ok {
-			return errors.New("Failed reading event")
-		}
-		log.Printf("Saving `%s` for `%s` on the `coins` Index", event.ID, event.Date)
-		ctx := context.Background()
-		data, err := json.Marshal(event)
-		if err != nil {
-			log.Fatal("Failed marshalling struct into JSON")
-			return err
-		}
-
-		req := esapi.IndexRequest{
-			Index:      "coins",
-			DocumentID: fmt.Sprintf("%s_%s", event.ID, event.Date),
-			Body:       bytes.NewReader(data),
-			Refresh:    "true",
-		}
-
-		res, err := req.Do(ctx, r.client)
-		if err != nil || res.IsError() {
-			log.Fatalf("Failed indexing the data for: %v", event)
-			return err
-		}
-		defer res.Body.Close()
-
-		log.Print("Successfully inserted the event into the `coins` index")
+func (r *Repository) Store(event *core.GamblerEvent) error {
+	log.Printf("Saving `%s` for `%s` on the `coins` Index", event.ID, event.Date)
+	ctx := context.Background()
+	data, err := json.Marshal(event)
+	if err != nil {
+		log.Fatal("Failed marshalling struct into JSON")
+		return err
 	}
+
+	req := esapi.IndexRequest{
+		Index:      "coins",
+		DocumentID: fmt.Sprintf("%s_%s", event.ID, event.Date),
+		Body:       bytes.NewReader(data),
+		Refresh:    "true",
+	}
+
+	res, err := req.Do(ctx, r.client)
+	if err != nil || res.IsError() {
+		log.Fatalf("Failed indexing the data for: %v", event)
+		return err
+	}
+	defer res.Body.Close()
+
+	log.Print("Successfully inserted the event into the `coins` index")
+	return nil
 }
